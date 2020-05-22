0abeff5d80142a20a7b86f6dd158a38c3ded24fb
Favini <darthjee@gmail.com>
2020-05-22 17:34:46 +0200

    Initial (#1)

---
+++ lib/magicka.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# @api public
+# @author darthjee
+class Magicka
+  autoload :VERSION, 'magicka/version'
+end
---
+++ spec/lib/magicka_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Magicka do
+  it 'does something' do
+  end
+end
