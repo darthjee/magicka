6838d234855dc43f4f75609b394c51ae8a53b1b4
darthjee <darthjee@gmail.com>
2020-06-02 18:52:54 +0200

    Use new template definition

--- lib/magicka/input.rb
+++ lib/magicka/input.rb
@@ -4,7 +4,5 @@ module Magicka
   # Input element representing an HTML +<input/>+
   class Input < InputElement
     with_attribute_locals :placeholder
-
-    template 'templates/forms/input'
   end
 end
