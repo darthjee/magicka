5616a26081cde022d957bf2241a9a3003441ed28
darthjee <darthjee@gmail.com>
2020-06-03 23:20:27 +0200

    Add doc

--- lib/magicka/button.rb
+++ lib/magicka/button.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module Magicka
+  # Button element representing an HTML +<button/>+
   class Button < Element
     with_attribute_locals :ng_click, :ng_disabled, :classes, :text
     template_folder 'templates/forms'
