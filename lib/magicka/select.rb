6f4a40e64cf48960cb799231bf3ea62e96f38d7e
darthjee <darthjee@gmail.com>
2020-06-03 22:17:50 +0200

    Add more specs

--- lib/magicka/select.rb
+++ lib/magicka/select.rb
@@ -4,4 +4,3 @@ module Magicka
   class Select < FormElement
   end
 end
-
--- spec/lib/magicka/form_element_spec.rb
+++ spec/lib/magicka/form_element_spec.rb
@@ -4,11 +4,14 @@ require 'spec_helper'
 
 describe Magicka::FormElement do
   let(:renderer) { instance_double('renderer') }
-  let(:template) { 'templates/forms/input' }
+  let(:template) { 'templates/forms/custom_template' }
 
-  let(:model)       { :my_model }
-  let(:field)       { :field }
-  let(:label)       { 'Label' }
+  let(:expected_template) { template }
+
+  let(:model) { :my_model }
+  let(:field) { :field }
+  let(:label) { 'Label' }
+  let(:klass) { Class.new(described_class) }
 
   let(:locals) do
     {
@@ -17,10 +20,6 @@ describe Magicka::FormElement do
       ng_errors: 'my_model.errors.field',
       ng_model: 'my_model.field'
     }
-  end
-
-  before do
-    described_class.template(template)
   end
 
   describe '.render' do
@@ -36,20 +35,52 @@ describe Magicka::FormElement do
     before do
       allow(renderer)
         .to receive(:render)
-        .with(partial: template, locals: locals)
+        .with(partial: expected_template, locals: locals)
     end
 
-    it do
-      described_class.render(arguments)
+    context 'when defined template' do
+      before do
+        klass.template(template)
+      end
 
-      expect(renderer).to have_received(:render)
-    end
-
-    context 'when called with extra params' do
       it do
-        described_class.render(name: 'Name', **arguments)
+        klass.render(arguments)
 
         expect(renderer).to have_received(:render)
+      end
+
+      context 'when called with extra params' do
+        it do
+          klass.render(name: 'Name', **arguments)
+
+          expect(renderer).to have_received(:render)
+        end
+      end
+    end
+
+    context 'when not defining a template' do
+      let(:method_builder)    { Sinclair.new(klass) }
+      let(:expected_template) { 'templates/forms/my_custom_element' }
+
+      before do
+        method_builder.add_class_method(:name) do
+          'Magicka::MyCustomElement'
+        end
+        method_builder.build
+      end
+
+      it do
+        klass.render(arguments)
+
+        expect(renderer).to have_received(:render)
+      end
+
+      context 'when called with extra params' do
+        it do
+          klass.render(name: 'Name', **arguments)
+
+          expect(renderer).to have_received(:render)
+        end
       end
     end
   end
