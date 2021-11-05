6528e3d118860b958e07765024017d68995f458c
darthjee <darthjee@gmail.com>
2021-11-05 22:16:01 +0100

    Add Docs for helper

--- lib/magicka/helper.rb
+++ lib/magicka/helper.rb
@@ -12,5 +12,44 @@ module Magicka
 
     with Form
     with Display
+
+    # @method self.with
+    #
+    # Adds a helper method magicka_(type)
+    #
+    # The created method executes a block with a an aggragator
+    #
+    # @param aggregator_class [Class<Magicka::Aggregator>] Agragator to be initialized
+    # @param type [String,Symbol] type of aggregator, this will define the method name
+    #
+    # @return Array<NilClass>
+
+    # @method magicka_form
+    #
+    # Execute a block with an aggregator focused on a model
+    #
+    # The aggregator renders elements as form elements
+    #
+    # @param model [String] Model to be processed
+    #
+    # @yield Render a form using the aggregator
+    #
+    # @see Magicka::Form
+    #
+    # @return [String]
+
+    # @method magicka_display
+    #
+    # Execute a block with an aggregator focused on a model
+    #
+    # The aggregator renders elements as display elements
+    #
+    # @param model [String] Model to be processed
+    #
+    # @yield Render a form using the aggregator
+    #
+    # @see Magicka::Display
+    #
+    # @return [String]
   end
 end
--- lib/magicka/helper/class_methods.rb
+++ lib/magicka/helper/class_methods.rb
@@ -2,7 +2,11 @@
 
 module Magicka
   module Helper
+    # Class methods for {Magicka::Helper}
     module ClassMethods
+      # (see Magicka::Helper.with)
+      #
+      # @param (see Magicka::Helper.with)
       def with(aggregator_class, type = aggregator_class.type)
         MethodBuilder.new(self).build_aggregator(aggregator_class, type)
       end
