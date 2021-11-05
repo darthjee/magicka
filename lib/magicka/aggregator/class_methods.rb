c975cad530eb871a0c933c04d701c4443900ea2a
darthjee <darthjee@gmail.com>
2021-11-05 22:35:41 +0100

    Add Aggregator::ClassMethods docs

--- lib/magicka/aggregator/class_methods.rb
+++ lib/magicka/aggregator/class_methods.rb
@@ -2,7 +2,16 @@
 
 module Magicka
   class Aggregator
+    # Class methods for {Magicka::Aggregator}
     module ClassMethods
+
+      # Configure an {Aggregator} adding a method to render an element
+      #
+      # @param element_class [Class<Magicka::ELement>]
+      #   Class of the element to be rendered
+      # @param method_name [String,Symbol]
+      #   Name of the method that will render the element
+      # @param template [String] custom template file to be used
       def with_element(element_class, method_name = nil, template: nil)
         MethodBuilder
           .new(self, element_class, method_name, template: template)
@@ -10,6 +19,7 @@ module Magicka
           .build
       end
 
+      # Set and return the type of the aggregator
       def type(*args)
         return @type ||= default_type if args.empty?
 
