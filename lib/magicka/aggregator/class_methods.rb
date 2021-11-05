3dde6768f570ce11ef2ae04b98e8d66568449f64
darthjee <darthjee@gmail.com>
2021-11-05 22:47:06 +0100

    Add type document

--- lib/magicka/aggregator/class_methods.rb
+++ lib/magicka/aggregator/class_methods.rb
@@ -4,7 +4,6 @@ module Magicka
   class Aggregator
     # Class methods for {Magicka::Aggregator}
     module ClassMethods
-
       # Configure an {Aggregator} adding a method to render an element
       #
       # @param element_class [Class<Magicka::ELement>]
@@ -20,8 +19,14 @@ module Magicka
       end
 
       # Set and return the type of the aggregator
-      def type(*args)
-        return @type ||= default_type if args.empty?
+      #
+      # When new_type is ommited, returns the last value
+      #
+      # @param new_type [Symbol,String] The value to be set
+      #
+      # @return [Symbol] the type of the aggregator
+      def type(new_type = nil)
+        return @type ||= default_type unless new_type
 
         @type = args.first.to_sym
       end
--- lib/magicka/helper/method_builder.rb
+++ lib/magicka/helper/method_builder.rb
@@ -2,7 +2,9 @@
 
 module Magicka
   module Helper
+    # Builds methods for {Magicka::Helper}
     class MethodBuilder < Sinclair
+      # (see Magicka::Helper.with)
       def build_aggregator(aggregator_class, type = aggregator_class.type)
         add_method("magicka_#{type}") do |model, &block|
           block.call(aggregator_class.new(self, model))
