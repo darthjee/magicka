4ececbc7324b5fc5a4f4e967a57423fce30473ca
darthjee <darthjee@gmail.com>
2023-05-25 13:24:53 -0300

    Test string definition

--- spec/lib/magicka/helper/aggregator_options_spec.rb
+++ spec/lib/magicka/helper/aggregator_options_spec.rb
@@ -13,7 +13,8 @@ describe Magicka::Helper::AggregatorOptions do
     }
   end
 
-  let(:aggregator) { Class.new(Magicka::Aggregator) }
+  let(:aggregator_class) { Class.new(Magicka::Aggregator) }
+  let(:aggregator)       { aggregator_class }
   let(:type)             { :my_aggregator }
   let(:config_block) do
     proc { with_element(Magicka::Input) }
@@ -54,6 +55,43 @@ describe Magicka::Helper::AggregatorOptions do
         it 'returns the given aggregator class' do
           expect(options.configured_aggregator)
             .to be(aggregator)
+        end
+      end
+    end
+
+    context 'when aggregator is a String' do
+      let(:aggregator)      { "Magicka::#{aggregator_name}" }
+      let(:aggregator_name) { :CustomAggregator }
+
+      before { Magicka.const_set(aggregator_name, aggregator_class) }
+
+      context 'when config block is given' do
+        let(:aggregator_name) { :CustomAggregator1 }
+
+        it 'returns the given aggregator class' do
+          expect(options.configured_aggregator)
+            .to be(aggregator_class)
+        end
+      end
+
+      context 'when config block is given and is called' do
+        let(:aggregator_name) { :CustomAggregator2 }
+
+        it 'configure the aggregator' do
+          options.configured_aggregator.new(renderer, model)
+            .input(:name)
+
+          expect(element_class).to have_received(:render)
+        end
+      end
+
+      context 'when config block is not given' do
+        let(:aggregator_name) { :CustomAggregator3 }
+        let(:config_block) { nil }
+
+        it 'returns the given aggregator class' do
+          expect(options.configured_aggregator)
+            .to be(aggregator_class)
         end
       end
     end
