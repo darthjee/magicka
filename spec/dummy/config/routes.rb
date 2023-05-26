95d56da4227a953b865f75bbd8b64a3949afcb18
darthjee <darthjee@gmail.com>
2023-05-26 06:22:20 -0300

    Rename contorller

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get '/web_form' => 'web_forms#show'
+  resources :documents
 end
--- spec/integration/yard/magicka/helper_spec.rb
+++ spec/integration/yard/magicka/helper_spec.rb
@@ -6,7 +6,9 @@ describe 'yard for Magicka::Helper' do
   describe '.with' do
     describe 'when passing only an aggregator' do
       it do
-        get '/web_form'
+        get '/documents/1'
+
+        expect(response.status).to eq(200)
       end
     end
   end
