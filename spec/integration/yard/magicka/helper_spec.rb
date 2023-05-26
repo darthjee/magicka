43be61164329f0c05f1368cea3a6e21af62a64fe
darthjee <darthjee@gmail.com>
2023-05-26 07:28:11 -0300

    Rubocop fix

--- spec/integration/yard/magicka/helper_spec.rb
+++ spec/integration/yard/magicka/helper_spec.rb
@@ -7,8 +7,6 @@ describe 'yard for Magicka::Helper' do
     describe 'when passing only an aggregator' do
       let(:document) { create(:document) }
 
-      before { get "/documents/#{document.id}" }
-
       let(:expected_body) do
         [
           '<dh>Name</dh>',
@@ -18,6 +16,8 @@ describe 'yard for Magicka::Helper' do
           "<dd>#{document.reference}</dd>"
         ].join(" *\n *")
       end
+
+      before { get "/documents/#{document.id}" }
 
       it do
         expect(response.status).to eq(200)
