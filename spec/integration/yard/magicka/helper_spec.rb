11faff71c3ba24d1a81234b5eff62aa1fb70cf5d
darthjee <darthjee@gmail.com>
2023-05-26 06:25:36 -0300

    Simple test

--- spec/integration/yard/magicka/helper_spec.rb
+++ spec/integration/yard/magicka/helper_spec.rb
@@ -5,9 +5,11 @@ require 'spec_helper'
 describe 'yard for Magicka::Helper' do
   describe '.with' do
     describe 'when passing only an aggregator' do
-      it do
-        get '/documents/1'
+      let(:document) { create(:document) }
 
+      before { get "/documents/#{document.id}" }
+
+      it do
         expect(response.status).to eq(200)
       end
     end
